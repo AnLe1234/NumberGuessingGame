114ad3c6c83b1b11be10b4072ba6f6f8959dd5cf
An Le <anle1@cpp.edu>
2022-10-21 22:22:15 -0700

    Initial outline

---
+++ NumberGuessingGame.c
@@ -0,0 +1,15 @@
+// program user guess the random correct number
+// initialize default value for max number
+// menu 1. play game 2. change max number 3. quit
+
+// option 1
+// loop until input == q
+    // if too high then out high
+    // if too low then out low
+
+// option 2
+// change default value
+
+// option 3
+// thank you for playing game
+// return 0
